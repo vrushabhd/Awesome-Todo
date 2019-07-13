72f37885c647bf42b5010d8c1d34c85717159159
Vrushabh Dhond <vrushabhdhond1907@gmail.com>
2019-07-13 23:02:25 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,79 @@
+
+// var h =document.createElement('h1')
+// var myValue =document.createTextNode('Hello World!')
+// h.appendChild(myValue);
+// document.querySelector('h1').appendChild(h)
+
+var ul =document.getElementById('list')
+var li;
+var p;
+
+
+
+var addButton = document.getElementById('add');
+addButton.addEventListener('click',addItem);
+
+var removeButton =document.getElementById('remove');
+removeButton.addEventListener('click',removeItem)
+var removeeveryButton =document.getElementById('removee')
+removeeveryButton.addEventListener('click',removeevery);
+
+
+function removeevery(){
+    li =ul.children
+    for (let index = 0; index < li.length; index++) {
+        
+               ul.removeChild(li[index])
+           }
+}
+
+
+
+function addItem(){
+    var input =document.getElementById('input');
+    var item = input.value;
+    ul= document.getElementById('list');
+    var textnode =document.createTextNode(item);
+    if (item ==='') {
+        return false;
+       
+    }else{
+        //create li
+        li =document.createElement('li');
+        //create checkbox
+        var checkbox =document.createElement('input')
+        checkbox.type ='checkbox';
+        checkbox.setAttribute('id','check');
+        //create label
+        var label = document.createElement('label')
+        label.setAttribute('for','item');//optional
+        
+        //add this element on web page 
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(textnode);
+        li.appendChild(label);
+        ul.insertBefore(li,ul.childNodes[0]);
+        setTimeout(() => {
+            li.className ='visual';
+        }, 2);
+        input.value ='';
+   
+ 
+ 
+    }
+     
+ 
+}
+function removeItem(){
+    li =ul.children
+    for (let index = 0; index < li.length; index++) {
+           while (li[index] &&li[index].children[0].checked) {
+               ul.removeChild(li[index])
+           }
+         
+    }
+  }
+   
+
+   
